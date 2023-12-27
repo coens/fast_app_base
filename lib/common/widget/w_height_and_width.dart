1bcaac8c10b38269c1b412caafacb41ef8d05f92
coens <coens@naver.com>
2023-12-27 16:37:25 +0900

    11. 혜택 탭

--- lib/common/widget/w_height_and_width.dart
+++ lib/common/widget/w_height_and_width.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
-final width10 = Width(10);
 final width5 = Width(5);
-final height10 = Height(10);
+final width10 = Width(10);
+final width20 = Width(20);
+final width30 = Width(30);
+
 final height5 = Height(5);
+final height10 = Height(10);
+final height20 = Height(20);
+final height30 = Height(30);
 
 
 
