950163b1a262b1fd7a51a5229fc2bf97a7ebb8ed
coens <coens@naver.com>
2023-12-31 17:35:40 +0900

    3.01. Intro. 가장 쉬운 Animation 코드 - 1

--- lib/screen/main/tab/home/w_ttoss_app_bar.dart
+++ lib/screen/main/tab/home/w_ttoss_app_bar.dart
@@ -57,7 +57,7 @@ class _TtossAppBarState extends State<TtossAppBar> {
                 )
 
               ],
-            ),
+            ).animate(onComplete: (controller) => controller.repeat(),).shake(duration: 2000.ms, hz:3).then().fadeOut(duration: 1000.ms),
           ),
         ],
       ),
