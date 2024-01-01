eabaee2b238d4e5944f8d1012b19ce3ebcd9fbf6
coens <coens@naver.com>
2024-01-01 19:47:52 +0900

    3.10. Rive-Flutter 인터렉티브 애니메이션 구현해보기 (움직이는 곰)

--- lib/screen/main/tab/stock/setting/d_number.dart
+++ lib/screen/main/tab/stock/setting/d_number.dart
@@ -1,6 +1,7 @@
 import 'package:fast_app_base/common/common.dart';
 import 'package:fast_app_base/common/widget/w_round_button.dart';
 import 'package:fast_app_base/common/widget/w_rounded_container.dart';
+import 'package:fast_app_base/screen/main/tab/stock/setting/w_text_watching_bear.dart';
 import 'package:flutter/material.dart';
 import 'package:nav/dialog/dialog.dart';
 import 'package:nav/enum/enum_nav_ani.dart';
@@ -13,26 +14,83 @@ class NumberDialog extends DialogWidget<int> {
 }
 
 class _NotificationDialogState extends DialogState<NumberDialog> {
-  final controller = TextEditingController();
+  final numberController = TextEditingController();
+  final passwordController = TextEditingController();
+  final numberFocus = FocusNode();
+  final passwordFocus = FocusNode();
+  final textBearController = TextWatchingBearController();
+
+  bool check = false;
+  bool handsUp = false;
+  double look = 0.0;
+
+  @override
+  void initState() {
+    numberController.addListener(() {
+      setState(() {
+        look = numberController.text.length.toDouble() * 5;
+      });
+    });
+
+    numberFocus.addListener(() {
+      setState(() {
+        check = numberFocus.hasFocus;
+      });
+    });
+
+    passwordFocus.addListener(() {
+      setState(() {
+        handsUp = passwordFocus.hasFocus;
+      });
+    });
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      type: MaterialType.transparency,
-      child: Column(
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           RoundedContainer(
             child: Column(
               children: [
                 "숫자를 입력해 주세요".text.make(),
-                TextField(controller: controller, keyboardType: TextInputType.number),
+                SizedBox(
+                  width: 200,
+                  height: 200,
+                  child: TextWatchingBear(
+                    check: check,
+                    handsUp: handsUp,
+                    look: look,
+                    controller: textBearController,
+                  ),
+                ),
+                TextField(
+                  controller: numberController,
+                  focusNode: numberFocus,
+                  keyboardType: TextInputType.number,
+                ),
+                TextField(
+                  obscureText: true,
+                  controller: passwordController,
+                  focusNode: passwordFocus,
+                  keyboardType: TextInputType.number,
+                ),
                 RoundButton(
                   text: "완료",
-                  onTap: () {
-                    final text = controller.text;
-                    int number = int.parse(text);
-                    widget.hide(number);
+                  onTap: () async {
+                    final text = numberController.text;
+                    try {
+                      int number = int.parse(text);
+                      textBearController.runSuccessAnimation();
+                      await sleepAsync(2.seconds);
+                      widget.hide(number);
+                    } catch (e) {
+                      textBearController.runFailAnimation();
+                    }
                   },
                 )
               ],
